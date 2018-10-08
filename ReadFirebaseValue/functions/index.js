0bf8e2330174bf18c8281a810c241790ee0d2f40
Gastón Saillen <gaston_luque_29@hotmail.com>
2018-10-07 22:57:39 -0300

    Update index.js

--- ReadFirebaseValue/functions/index.js
+++ ReadFirebaseValue/functions/index.js
@@ -1,14 +1,15 @@
 const functions = require('firebase-functions'); 
 const admin = require('firebase-admin'); 
-admin.initializeApp(); //init firebase admin
+admin.initializeApp(); //Iniciar firebase Admin para usar Firebase
+
+
+//Funcion que chequea por datos en un nodo de la base de datos , tanto antes como despues de cambiar su valor
 
     exports.dbWrite = functions.database.ref('/Prueba/nombre').onWrite((change, context) => {
-        const beforeData = change.before.val(); // data before the write
+        const beforeData = change.before.val(); // data antes de cambiar el valor
         console.log('Nombre antes de cambiar: ', beforeData);
-        const afterData = change.after.val(); // data after the write
+        const afterData = change.after.val(); // data despues de cambiar el valor
         console.log('Nombre despues de cambiar: ', afterData);
-
-        //para escribir algo a la base de datos uso promesas y tengo que retornarlas y catchearlas
 
         
       });
